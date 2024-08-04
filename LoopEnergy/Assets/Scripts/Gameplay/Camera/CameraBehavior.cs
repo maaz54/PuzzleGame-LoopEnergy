f9a6ead283c381ad5ca2e6fbba2895d7dc7f3312
maaz54 <maaz.ali.khan@coinerz.com>
2024-08-04 18:49:55 +0500

    feat: added Camera Shake Effect

--- LoopEnergy/Assets/Scripts/Gameplay/Camera/CameraBehavior.cs
+++ LoopEnergy/Assets/Scripts/Gameplay/Camera/CameraBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace EnergyLoop.Game.Gameplay.Manager
@@ -11,6 +12,32 @@ namespace EnergyLoop.Game.Gameplay.Manager
         public void AdjustCameraSize(int gridSizeX, int gridSizeY)
         {
             Camera.orthographicSize = gridSizeX > gridSizeY ? gridSizeX + 2 : gridSizeY + 2;
+            originalPos = transform.localPosition;
+        }
+
+        public float shakeDuration = 0.5f;
+        public float shakeMagnitude = 0.1f;
+
+        private Vector3 originalPos;
+
+        [ContextMenu("TriggerShake")]
+        public void TriggerShake()
+        {
+            _ = Shake();
+        }
+
+        private async Task Shake()
+        {
+            float elapsed = 0.0f;
+
+            while (elapsed < shakeDuration)
+            {
+                transform.localPosition = originalPos + (Vector3)Random.insideUnitCircle * shakeMagnitude;
+                elapsed += Time.deltaTime;
+                await Task.Yield();
+            }
+
+            transform.localPosition = originalPos;
         }
     }
 
